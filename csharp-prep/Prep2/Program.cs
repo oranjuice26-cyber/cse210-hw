0acbbf5242e1a5f17d4e42bb6fb1d135977a6c93
Oran Jackson <oran.juice26@gmail.com>
2026-01-10 17:41:17 -0700

    Completed the task

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your percentage grade? ");
+        string gradeInText = Console.ReadLine();
+        int grade = int.Parse(gradeInText);
+        string letterGrade = "";
+        if (grade < 60)
+        {
+            letterGrade = "F";
+        } 
+        else if (grade < 70)
+        {
+            letterGrade = "D";
+        }
+        else if (grade < 80)
+        {
+            letterGrade = "C";
+        }
+        else if (grade < 90)
+        {
+            letterGrade = "B";
+        }
+        else
+        {
+            letterGrade = "A";
+        }
+        Console.WriteLine(letterGrade);
+        if (grade >= 70)
+        {
+            Console.Write("You passed! Good Job!");
+        }
+        else
+        {
+            Console.Write("You did not pass. I am sure you will next time!");
+        }
     }
 }
