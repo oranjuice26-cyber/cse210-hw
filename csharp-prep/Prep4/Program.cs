abd5bce0ed547218b22dbb9a9b1d19f1d5ba409d
Oran Jackson <oran.juice26@gmail.com>
2026-01-15 16:50:49 -0700

    Finished task

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,33 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Add a number to a list, type 0 when finished.");
+        int aNum = -1;
+        int sum = 0;
+        int large = 0;
+        while(aNum != 0){
+            Console.WriteLine("Enter number: ");
+            aNum = int.Parse(Console.ReadLine());
+            numbers.Add(aNum);
+        }
+        foreach(int number in numbers)
+        {
+            sum = sum + number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        float mean = (float)sum / (numbers.Count-1);
+        Console.WriteLine($"The average is: {mean} ");
+        foreach(int number in numbers)
+        {
+            if (number > large)
+            {
+                large = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {large}");
     }
 }
