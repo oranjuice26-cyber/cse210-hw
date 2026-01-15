723f343ba9679f748472930a803fa800ee86c5cb
Oran Jackson <oran.juice26@gmail.com>
2026-01-15 16:31:06 -0700

    Finished task

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int hiddenNum = randomGenerator.Next(1,101);
+        int guessInt = -1;
+        while(guessInt != hiddenNum)
+        {
+            Console.WriteLine("Guess the number: ");
+            string guessStr = Console.ReadLine();
+            guessInt = int.Parse(guessStr);
+            if (guessInt == hiddenNum)
+            {
+                Console.WriteLine("You guessed it!");
+            } else if (guessInt > hiddenNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }            
+        }
+
     }
 }
