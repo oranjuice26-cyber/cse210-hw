8c92efcec1aa5acdfc1f915be80cc8874bb0285b
Oran Jackson <oran.juice26@gmail.com>
2026-01-22 17:19:31 -0700

    Added the classes and completed the assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Vice President of Marketing";
+        job1._company = "Coca Cola";
+        job1._startYear = 2019;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "President of Marketing";
+        job2._company = "Pepsi";
+        job2._startYear = 2024;
+        job2._endYear = 2026;
+
+        Resume exampleResume = new Resume();
+        exampleResume._name = "Lova O. Soda";
+
+        exampleResume._jobs.Add(job1);
+        exampleResume._jobs.Add(job2);
+
+        exampleResume.Display();
     }
 }
